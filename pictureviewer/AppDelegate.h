1140734e7175e3df0c13a059ffcdc5397923f052
_noname <zxononame@gmail.com>
2020-06-21 15:22:12 +0800

    Initial Commit

---
+++ pictureviewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pictureviewer
+//
+//  Created by zxononame on 2020/6/21.
+//  Copyright © 2020 zxononame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
